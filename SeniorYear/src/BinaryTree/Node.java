92ac2b62a50e4f201e710e8b8daa716fef8be77f
nmd1 <rafty132@gmail.com>
2015-03-09 09:59:24 -0400

    Node for Binary Program
    The Binary Node

--- SeniorYear/src/BinaryTree/Node.java
+++ SeniorYear/src/BinaryTree/Node.java
@@ -5,10 +5,32 @@
  */
 package BinaryTree;
 
-/**
- *
- * @author s11370863
- */
 public class Node {
+    Node left;
+    Node right;
+    Object o;
     
+    public Node getLeft() {
+        return left;
+    }
+    public Node getRight() {
+        return right;
+    }
+    public Object getObject() {
+        return o();     
+    }
+
+    private Object o() {
+        return right;
+    }
+    
+    public void setLeft(Node l) {
+        left = l;
+    }
+    public void setRight(Node r) {
+        right = r;
+    }
+    public void setObject(Object or){
+        o = or;
+    }
 }
